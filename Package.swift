1c26ab13eaa3cf88e1a167e2351934c16a383738
Kazimir Makaranka <m2014ky@gmail.com>
2024-10-14 12:47:45 +0300

    MongoDB

--- Package.swift
+++ Package.swift
@@ -11,8 +11,8 @@ let package = Package(
         .package(url: "https://github.com/vapor/vapor.git", from: "4.99.3"),
         // 🗄 An ORM for SQL and NoSQL databases.
         .package(url: "https://github.com/vapor/fluent.git", from: "4.9.0"),
-        // 🐬 Fluent driver for MySQL.
-        .package(url: "https://github.com/vapor/fluent-mysql-driver.git", from: "4.4.0"),
+        // 🌱 Fluent driver for Mongo.
+        .package(url: "https://github.com/vapor/fluent-mongo-driver.git", from: "1.3.1"),
         // 🔵 Non-blocking, event-driven networking for Swift. Used for custom executors
         .package(url: "https://github.com/apple/swift-nio.git", from: "2.65.0"),
     ],
@@ -21,7 +21,7 @@ let package = Package(
             name: "App",
             dependencies: [
                 .product(name: "Fluent", package: "fluent"),
-                .product(name: "FluentMySQLDriver", package: "fluent-mysql-driver"),
+                .product(name: "FluentMongoDriver", package: "fluent-mongo-driver"),
                 .product(name: "Vapor", package: "vapor"),
                 .product(name: "NIOCore", package: "swift-nio"),
                 .product(name: "NIOPosix", package: "swift-nio"),
@@ -44,3 +44,4 @@ var swiftSettings: [SwiftSetting] { [
     .enableUpcomingFeature("DisableOutwardActorInference"),
     .enableExperimentalFeature("StrictConcurrency"),
 ] }
+
--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -1,6 +1,6 @@
 import NIOSSL
 import Fluent
-import FluentMySQLDriver
+import FluentMongoDriver
 import Vapor
 
 // configures your application
@@ -8,17 +8,10 @@ public func configure(_ app: Application) async throws {
     // uncomment to serve files from /Public folder
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
 
-   // 2
-    app.databases.use(.mysql(
-      hostname: Environment.get("DATABASE_HOST") ?? "localhost",
-      username: Environment.get("DATABASE_USERNAME")
-        ?? "vapor_username",
-      password: Environment.get("DATABASE_PASSWORD")
-        ?? "vapor_password",
-      database: Environment.get("DATABASE_NAME")
-        ?? "vapor_database",
-      tlsConfiguration: .forClient(certificateVerification: .none)
-    ), as: .mysql) 
+    try app.databases.use(DatabaseConfigurationFactory.mongo(
+        connectionString: Environment.get("DATABASE_URL") ?? "mongodb://localhost:27017/vapor_database"
+    ), as: .mongo)
+
    // 1
   app.migrations.add(CreateAcronym())
     
